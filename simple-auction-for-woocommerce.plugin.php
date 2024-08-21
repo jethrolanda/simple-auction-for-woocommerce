53f844d5b4b25feda09745a144deef52fb6801ab
Jethro Landa <jemaq_lan@hotmail.com>
2024-08-22 00:18:04 +0800

    add custom product tab called auction. add product tab content fields

--- simple-auction-for-woocommerce.plugin.php
+++ simple-auction-for-woocommerce.plugin.php
@@ -36,6 +36,9 @@ class Simple_Auction_For_WooCommerce
     add_action('woocommerce_loaded', array($this, 'wc_custom_produt'));
     add_filter('product_type_selector', array($this, 'auction_custom_product_type'));
     add_action('woocommerce_product_data_panels', array($this, 'wkwc_add_product_data_tab_content'));
+
+    add_filter('woocommerce_product_data_tabs', array($this, 'uwa_custom_product_tabs'));
+    add_action('woocommerce_product_data_panels', array($this, 'uwa_options_product_tab_content'));
   }
 
   /**
@@ -96,6 +99,129 @@ class Simple_Auction_For_WooCommerce
             'value'       => $product_object->get_meta('_wkwc_name', true),
             'default'     => '',
             'placeholder' => esc_html__('Enter your name', 'wkcp'),
+          )
+        );
+        ?>
+      </div>
+    </div>
+  <?php
+  }
+
+  /**
+   * Add a custom product tab.
+   *
+   * @package Ultimate WooCommerce Auction
+   * @author Nitesh Singh
+   * @since 1.0
+   */
+  public function uwa_custom_product_tabs($product_data_tabs)
+  {
+    $auction_tab = array(
+      'auction_tab' => array(
+        'label'  => __('Auction', 'simple-auction-for-woocommerce'),
+        'target' => 'auction_options',
+        'class'  => array('show_if_auction', 'hide_if_grouped', 'hide_if_external', 'hide_if_variable', 'hide_if_simple'),
+      ),
+    );
+
+    // unset($product_data_tabs['inventory']);
+    // error_log(print_r($auction_tab + $product_data_tabs, true));
+    return $auction_tab + $product_data_tabs;
+  }
+
+  /**
+   * Contents of the Auction  Product options product tab.
+   *
+   * @package Ultimate WooCommerce Auction
+   * @author Nitesh Singh
+   * @since 1.0
+   */
+  public function uwa_options_product_tab_content()
+  {
+    global $post;
+    $product = \wc_get_product($post->ID);
+  ?>
+    <div id='auction_options' class='panel woocommerce_options_panel'>
+      <div class='options_group'>
+        <?php
+        \woocommerce_wp_select(
+          array(
+            'id'      => 'woo_ua_product_condition',
+            'label'   => __('Auction Type', 'simple-auction-for-woocommerce'),
+            'options' => apply_filters('ultimate_woocommerce_auction_product_condition', array(
+              'new'  => __('English Auction', 'simple-auction-for-woocommerce'),
+              'used' => __('Dutch Auction', 'simple-auction-for-woocommerce'),
+            )),
+            'desc_tip' => true,
+            'description' => __('Set the price where the price of the product will start from.', 'ultimate-woocommerce-auction'),
+          )
+        );
+        \woocommerce_wp_text_input(
+          array(
+            'id'                => 'woo_ua_opening_price',
+            'label'             => __('Starting Price', 'ultimate-woocommerce-auction') . ' (' . get_woocommerce_currency_symbol() . ')',
+            'desc_tip'          => 'true',
+            'description'       => __('Set the price where the price of the product will start from.', 'ultimate-woocommerce-auction'),
+            'data_type'         => 'price',
+            'custom_attributes' => array(
+              'step' => 'any',
+              'min'  => '0',
+            ),
+          )
+        );
+        \woocommerce_wp_text_input(
+          array(
+            'id'                => 'woo_ua_opening_price',
+            'label'             => __('Lowest Price', 'ultimate-woocommerce-auction') . ' (' . get_woocommerce_currency_symbol() . ')',
+            'desc_tip'          => 'true',
+            'description'       => __('Set the price where the price of the product will start from.', 'ultimate-woocommerce-auction'),
+            'data_type'         => 'price',
+            'custom_attributes' => array(
+              'step' => 'any',
+              'min'  => '0',
+            ),
+          )
+        );
+        \woocommerce_wp_text_input(
+          array(
+            'id'                => 'woo_ua_opening_price',
+            'label'             => __('Increment', 'ultimate-woocommerce-auction'),
+            'desc_tip'          => 'true',
+            'description'       => __('Set the price where the price of the product will start from.', 'ultimate-woocommerce-auction'),
+            'data_type'         => 'price',
+            'type' => 'number',
+            'custom_attributes' => array(
+              'step' => 'any',
+              'min'  => '0',
+            ),
+          )
+        );
+        \woocommerce_wp_text_input(
+          array(
+            'id'                => 'woo_ua_opening_price',
+            'label'             => __('Start Date', 'ultimate-woocommerce-auction'),
+            'desc_tip'          => 'true',
+            'description'       => __('Set the price where the price of the product will start from.', 'ultimate-woocommerce-auction'),
+            'data_type'         => 'price',
+            'type' => 'number',
+            'custom_attributes' => array(
+              'step' => 'any',
+              'min'  => '0',
+            ),
+          )
+        );
+        \woocommerce_wp_text_input(
+          array(
+            'id'                => 'woo_ua_opening_price',
+            'label'             => __('End Date', 'ultimate-woocommerce-auction'),
+            'desc_tip'          => 'true',
+            'description'       => __('Set the price where the price of the product will start from.', 'ultimate-woocommerce-auction'),
+            'data_type'         => 'price',
+            'type' => 'number',
+            'custom_attributes' => array(
+              'step' => 'any',
+              'min'  => '0',
+            ),
           )
         );
         ?>
